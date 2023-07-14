f2ccd2509c27cd4c3d3d591ba2b15c4a0e4a1e01
Ullash Bhattacharjee <ullash.bhattacharjee@bjitacademy.com>
2023-07-14 20:44:52 +0600

    fixing apply security

--- src/main/java/com/bjit/traineeselectionsystem/repository/ApplicantRepository.java
+++ src/main/java/com/bjit/traineeselectionsystem/repository/ApplicantRepository.java
@@ -1,7 +1,11 @@
 package com.bjit.traineeselectionsystem.repository;
 
+import com.bjit.traineeselectionsystem.entity.AdmitCardEntity;
 import com.bjit.traineeselectionsystem.entity.ApplicantEntity;
+import com.bjit.traineeselectionsystem.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ApplicantRepository extends JpaRepository<ApplicantEntity , Long> {
+
+    ApplicantEntity findByUser(UserEntity userEntity);
 }
--- src/main/java/com/bjit/traineeselectionsystem/service/impl/ApplicantServiceImpl.java
+++ src/main/java/com/bjit/traineeselectionsystem/service/impl/ApplicantServiceImpl.java
@@ -3,15 +3,21 @@ package com.bjit.traineeselectionsystem.service.impl;
 import com.bjit.traineeselectionsystem.entity.ApplicantEntity;
 import com.bjit.traineeselectionsystem.entity.ApplyEntity;
 import com.bjit.traineeselectionsystem.entity.JobCircularEntity;
+import com.bjit.traineeselectionsystem.entity.UserEntity;
 import com.bjit.traineeselectionsystem.model.ApplyRequest;
 import com.bjit.traineeselectionsystem.model.Response;
 import com.bjit.traineeselectionsystem.repository.ApplicantRepository;
 import com.bjit.traineeselectionsystem.repository.ApplyRepository;
 import com.bjit.traineeselectionsystem.repository.JobCircularRepository;
+import com.bjit.traineeselectionsystem.repository.UserRepository;
 import com.bjit.traineeselectionsystem.service.ApplicantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 
 @Service
@@ -21,9 +27,30 @@ public class ApplicantServiceImpl implements ApplicantService {
     private final ApplicantRepository applicantRepository;
     private final JobCircularRepository jobCircularRepository;
     private final ApplyRepository applyRepository;
+    private final UserRepository userRepository;
 
     @Override
     public ResponseEntity<Response<?>> apply(ApplyRequest applyRequest) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Long loggedInApplicantId = ((UserEntity) authentication.getPrincipal()).getUserId(); // 23
+
+        UserEntity user = userRepository.findById(loggedInApplicantId)
+                .orElseThrow(()->new IllegalArgumentException("User not found"));
+
+        //Applicant --> user(23) -- > email
+
+        ApplicantEntity applicant= applicantRepository.findByUser(user);
+
+        //System.out.println(loggedInApplicantId);
+
+        // Check if the applicantId from the ApplyRequest matches the logged-in user's applicantId
+        if (!applyRequest.getApplicantId().equals(applicant.getApplicantId())) {
+            throw new IllegalArgumentException("Invalid applicant ID");
+            // or return an error response indicating that the applicant ID does not match the logged-in user
+        }
+
+
 
         // Retrieve the applicant by applicantId from the applyRequest
         ApplicantEntity applicantEntity = applicantRepository.findById(applyRequest.getApplicantId())
