8d4fdeaee09bb53d4e306df1059a4fd8f2b988c4
turjo998 <ullash.bhattacharjee@bjitacademy.com>
2023-07-15 11:42:34 +0600

    testing commit

---
+++ src/main/java/com/bjit/traineeselectionsystem/controller/RandomController.java
@@ -0,0 +1,10 @@
+package com.bjit.traineeselectionsystem.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RandomController {
+
+    //sample text for testing
+}
